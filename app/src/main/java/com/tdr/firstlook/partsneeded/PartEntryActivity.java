b8866298a5aa8b163e20a82223e5a2f1dbf44793
Terrence Rodgers <tdr0082@gmail.com>
2019-07-13 11:30:52 -0500

    Add PartEntryActivity and generate layout

---
+++ app/src/main/java/com/tdr/firstlook/partsneeded/PartEntryActivity.java
@@ -0,0 +1,14 @@
+package com.tdr.firstlook.partsneeded;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PartEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_part_entry);
+    }
+}
