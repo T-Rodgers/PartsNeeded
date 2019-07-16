c8747cdd6cdddad0aa5e7eb200a63b76df07c1e0
Terrence Rodgers <tdr0082@gmail.com>
2019-07-15 19:18:19 -0500

    Implement "setSendType" method

--- app/src/main/java/com/tdr/firstlook/partsneeded/PartEntryActivity.java
+++ app/src/main/java/com/tdr/firstlook/partsneeded/PartEntryActivity.java
@@ -1,14 +1,41 @@
 package com.tdr.firstlook.partsneeded;
 
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class PartEntryActivity extends AppCompatActivity {
+
+    private Button sendButton;
+    private Button addBUtton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_part_entry);
+
+
+        sendButton = findViewById(R.id.sendButton);
+        addBUtton = findViewById(R.id.addButton);
+
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null) {
+            String sendType = extras.getString("button_text");
+
+            setSendType(sendType);
+        }
+
+    }
+
+    public void setSendType(String sendType) {
+
+        if (sendType.equals("Send Text")) {
+            sendButton.setText(sendType);
+        } else if (sendType.equals("Email")) {
+            sendButton.setText(sendType);
+        }
+
     }
 }
