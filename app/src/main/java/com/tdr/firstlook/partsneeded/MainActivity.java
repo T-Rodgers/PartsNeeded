fa3c6b44b08daa69ec6b79e0d34e338834286504
Terrence Rodgers <tdr0082@gmail.com>
2019-10-02 12:50:42 -0500

    Implement background gradient animation

--- app/src/main/java/com/tdr/firstlook/partsneeded/MainActivity.java
+++ app/src/main/java/com/tdr/firstlook/partsneeded/MainActivity.java
@@ -1,11 +1,13 @@
 package com.tdr.firstlook.partsneeded;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +17,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ConstraintLayout mainActivityLayout = findViewById(R.id.main_layout);
+
+        AnimationDrawable animation = (AnimationDrawable) mainActivityLayout.getBackground();
+        animation.setEnterFadeDuration(10);
+        animation.setExitFadeDuration(2500);
+        animation.start();
 
         Button smsButton = findViewById(R.id.smsButtonId);
         Button emailButton = findViewById(R.id.emailButtonId);
